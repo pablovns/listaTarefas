bed304e36264ff5cf8f0f389e31d9aaa74f195b7
pablovns <viniciuspablo962@gmail.com>
2022-09-15 12:05:06 -0300

    implementar a gravação de dados

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const frm = document.querySelector("form");
 const divQuadro = document.getElementById("divQuadro");
 
-
 frm.addEventListener("submit", (e) => {
     e.preventDefault(); // evita o envio do formulário vazio
     const tarefa = frm.inTarefa.value; // obtém o conteúdo digitado
@@ -61,7 +60,7 @@ frm.btRetirar.addEventListener("click", () => {
     });
 
     // resetar o contador da tarefa selecionada
-    tarefas.forEach(element => {
+    tarefas.forEach((element) => {
         element.setAttribute("class", "tarefaNormal");
     });
 
@@ -70,5 +69,37 @@ frm.btRetirar.addEventListener("click", () => {
 });
 
 frm.btGravar.addEventListener("click", () => {
-    
+    const tarefas = document.querySelectorAll("h5");
+
+    if (tarefas.length == 0) {
+        alert("Não há tarefas para salvar.");
+    }
+
+    let dados = ""; // string para "acumular" os dados
+
+    tarefas.forEach((tarefa) => {
+        dados += tarefa.innerText + ";";
+    });
+
+    // gravar no localStorage
+    localStorage.setItem("tarefasDia", dados.slice(0, -1));
+
+    if (localStorage.getItem("tarefasDia")) {
+        alert("Tarefas salvas.");
+    }
+});
+
+window.addEventListener("load", () => {
+    if (localStorage.getItem("tarefasDia")) {
+        // carrega as tarefas salvas no localStorage
+        const dados = localStorage.getItem("tarefasDia").split(";");
+
+        // cria os elementos a partir dos dados carregados
+        dados.forEach((dado) => {
+            const h5 = document.createElement("h5");
+            const texto = document.createTextNode(dado);
+            h5.appendChild(texto);
+            divQuadro.appendChild(h5);
+        });
+    }
 });
